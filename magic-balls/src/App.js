b38bf650559d0d5d54f140950864d45d4a696404
ramasrini1 <ramasrini1@gmail.com>
2022-05-27 11:12:00 -0700

    magicballGame

---
+++ magic-balls/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import EightBall from './EightBall'
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall />
+      <EightBall />
+      <EightBall />
+      <EightBall />
+      <EightBall />
+      <EightBall />
+      <EightBall />
+      <EightBall />
+     
+    </div>
+  );
+}
+
+export default App;
